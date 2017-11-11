c4d984b41ba5bb68fb90935605fcebdd4026930f
ahui666 <772084015@qq.com>
2017-11-11 15:55:18 +0800

    spider练习

---
+++ justdo/sp.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+browser=webdriver.PhantomJS()
+browser.get('http://www.baidu.com')
+
+# print(browser.page_source)
+# btn=browser.find_element_by_id('su')
+# btn.click()
+
+lis=browser.find_elements_by_css_selector('a')
+
+
+print(lis)
+browser.close()
